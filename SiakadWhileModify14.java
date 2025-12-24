fd07be6572eadae71bc312a8e072fdfe751ef90c
Hikmal Fadhillah Rasyid Andro Mulyawan <hikmalfadillahrasyid@gmail.com>
2025-12-24 12:30:59 +0700

    Percobaan 2 Pertanyaan 4 & 5
    Percobaan 2 Pertanyaan 4 & 5

---
+++ SiakadWhileModify14.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class SiakadWhileModify14 {
+    public static void main(String[] args) {
+        Scanner hikmal = new Scanner(System.in);
+        int i=0, nilai, jml;
+
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        jml = hikmal.nextInt();
+
+        while(i < jml){
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + ": ");
+            nilai = hikmal.nextInt();
+            
+            if(nilai <0 || nilai >100){
+                System.out.println("Nilai tidak valid, masukkan lagi nilai yang valid!");
+                continue;
+            }
+
+            if (nilai>80 && nilai<=100) {
+                System.out.println("Nilai mahasiswa ke-" +(i+1) + " adalah A");
+                System.out.println("Bagus! Pertahankan nilainya.");
+            } else if (nilai>73 && nilai<=80) {
+                System.out.println("Nilai mahasiswa ke-" +(i+1) + " adalah B+");
+            } else if (nilai>65 && nilai<=73) {
+                System.out.println("Nilai mahasiswa ke-" +(i+1) + " adalah B");
+            } else if (nilai>60 && nilai<=65) {
+                System.out.println("Nilai mahasiswa ke-" +(i+1) + " adalah C+");
+            } else if (nilai>50 && nilai<=60) {
+                System.out.println("Nilai mahasiswa ke-" +(i+1) + " adalah C");
+            } else if (nilai>39 && nilai<=50) {
+                System.out.println("Nilai mahasiswa ke-" +(i+1) + " adalah D");
+            } else {
+                System.out.println("Nilai mahasiswa ke-" +(i+1) + " adalah E");
+            }
+            i++;
+        }
+    }
+}
